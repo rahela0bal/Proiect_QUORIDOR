e534d2256ab0f60def530e170324a09c168df474
Rahela <rahela.balta@emanuel.ro>
2025-05-04 14:56:52 +0300

    Fix #10: am adaugat functia de creare casutelor si am apelat-o

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ function setup() {
   function draw() { 
     stroke('#e9a2f7');
     fill('#fad1f8'); 
-    
+
     square(452, 30, 700);
+
+    casuteMici(11, 466, 45, 12);
+    
   }
+
+  function casuteMici(nr,startX, startY,rand){
+    stroke('#e9a2f7');
+    fill('#f5f5dc');
+    let size = 51;
+        for( let ii = 0; ii < rand; ii++){
+            for ( let i=0; i<=nr; i++){
+            let x = startX + i * (size + 5);
+            let y = startY + ii * (size + 5);
+                square(x, y, size);
+        }
+    }
+}
